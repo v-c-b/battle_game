824d3148dda90d895fd21423db3515066d6e39c2
Rafaela <rafalucy3@gmail.com>
2019-09-03 16:34:40 +0100

    Add player name form and test to feature it

--- app.rb
+++ app.rb
@@ -7,16 +7,25 @@ set    :session_secret, "here be dragons"
 
 class Battle_app < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1_name = params[:Player1_Name]
-    @player_2_name = params[:Player2_Name]
-    erb :play  end
+    session[:Player1_Name] = params[:Player1_Name]
+    session[:Player2_Name]= params[:Player2_Name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @Player1_Name = session[:Player1_Name]
+    @Player2_Name = session[:Player2_Name]
+    erb :play
+  end
+  
 
   run! if app_file == $0
-
 
 end
